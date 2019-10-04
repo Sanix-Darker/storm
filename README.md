5a160f33c44949ec4170508be1192e7c677a8890
sanix-darker <s4nixd@gmail.com>
2019-10-04 09:55:18 +0100

    Official first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SToRM
+
+Stream your local videos/films/series in a LAN network.
+
+## Why you need Storm
+
+
+## Requirements
+
+
+## How it's work
+
+
+## How to use
+
+
+## Author
+
+- Sanix-darker
---
+++ client/assets/js/script.js
@@ -0,0 +1,106 @@
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.player-video');
+const progress = player.querySelector('.progress');
+const progressFilled = player.querySelector('.filled-progress');
+const toggle = player.querySelector('.toggle-play');
+const skippers = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player-slider');
+const fullscreen = player.querySelectorAll('#fullscreen')[0];
+
+// Logic
+// Play and pause of the video
+const togglePlay = () => {
+  if( window.innerHeight !== screen.height) {
+    const playState = video.paused ? 'play' : 'pause';
+    video[playState](); // Call play or paused method
+  }
+}
+
+// To toggle the fullscreen
+const toggleFullscreen = () => {
+  const p = video;
+  if( window.innerHeight !== screen.height) {
+    // browser is not fullscreen
+    let fn_enter = video.requestFullscreen || video.webkitRequestFullscreen || video.mozRequestFullScreen || video.oRequestFullscreen || video.msRequestFullscreen;
+    fn_enter.call(video);
+  } else {
+      let fn_exit = video.exitFullScreen || video.webkitExitFullScreen || video.mozExitFullScreen || video.oExitFullScreen || video.msExitFullScreen;
+      fn_exit.call(video);
+  }
+}
+
+const updateButton = () => {
+  const togglePlayBtn = document.querySelector('.toggle-play');
+  if (this.paused) {
+    togglePlayBtn.innerHTML = `<svg class="" width="16" height="16" viewBox="0 0 16 16"><title>play</title><path d="M3 2l10 6-10 6z"></path></svg>`;
+  } else {
+    togglePlayBtn.innerHTML = `<svg width="16" height="16" viewBox="0 0 16 16"><title>pause</title><path d="M2 2h5v12H2zm7 0h5v12H9z"></path></svg>`;
+  }
+}
+
+const skip = () => {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+const rangeUpdate = () => {
+  video[this.name] = this.value;
+}
+
+const progressUpdate = () =>  {
+  const percent = video.currentTime / video.duration * 100;
+  progressFilled.style.flexBasis = `${percent}%`;
+}
+
+const scrub = (e) => {
+  const scrubTime = e.offsetX / progress.offsetWidth * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// Event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', progressUpdate);
+toggle.addEventListener('click', togglePlay);
+fullscreen.addEventListener('click', toggleFullscreen);
+
+
+skippers.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', rangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', rangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+
+const Filter = () => {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('search_box');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+const scrollToBottom = (id) => {
+  var myDiv = document.getElementById(id);
+  window.scrollTo(0, myDiv.innerHeight);
+  setTimeout(() => {
+    var myDiv = document.getElementById(id);
+    window.scrollTo(0, myDiv.innerHeight);
+  }, 100)
+}
